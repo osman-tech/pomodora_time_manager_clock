48e52027ba16db9919802777c7fdf5062bfd03c4
Rahim Osman <rahimosman95@gmail.com>
2023-07-24 18:03:49 +0300

    completed my time manager project

--- main.py
+++ main.py
@@ -13,10 +13,53 @@ LONG_BREAK_MIN = 20
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset():
+    global reps
+    window.after_cancel(timer)
+    timer_label.config(text="timer")
+    canvas.itemconfig(timer_text, text="00:00")
+    check_marks.config(text="")
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    countdown(5 * 60)
+    global reps
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    reps += 1
+    if reps == 1:
+        countdown(work_sec)
+        timer_label.config(text="working time")
+        check_marks.config(text="_/")
+    if reps == 2:
+        countdown(short_break_sec)
+        timer_label.config(text="short break")
+    if reps == 3:
+        countdown(work_sec)
+        timer_label.config(text="working time")
+        check_marks.config(text="_/_/")
+    if reps == 4:
+        countdown(short_break_sec)
+        timer_label.config(text="short break")
+    if reps == 5:
+        countdown(work_sec)
+        timer_label.config(text="working time")
+        check_marks.config(text="_/_/_/")
+    if reps == 6:
+        countdown(short_break_sec)
+        timer_label.config(text="short break")
+    if reps == 7:
+        countdown(work_sec)
+        timer_label.config(text="working time")
+        check_marks.config(text="_/_/_/_/")
+    if reps == 8:
+        countdown(long_break_sec)
+        timer_label.config(text="long break")
+
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def countdown(count):
@@ -26,7 +69,11 @@ def countdown(count):
         count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, countdown, count -1)
+        global timer
+        timer = window.after(100, countdown, count - 1)
+    if count <= 0:
+        start_timer()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -35,13 +82,11 @@ window.title("Pamadoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 window.after(1000, )
 
-
 canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomatoe_img = tkinter.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomatoe_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
-
 
 timer_label = tkinter.Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
 timer_label.grid(column=1, row=0)
@@ -49,12 +94,10 @@ timer_label.grid(column=1, row=0)
 start_button = tkinter.Button(text="Start", fg="black", command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = tkinter.Button(text="Reset", fg="black")
+reset_button = tkinter.Button(text="Reset", fg="black", command=reset)
 reset_button.grid(column=2, row=2)
 
-check_marks = tkinter.Label(text="_/", fg=GREEN, bg=YELLOW)
+check_marks = tkinter.Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=2)
-
-
 
 window.mainloop()
